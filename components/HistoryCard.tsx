aec96299e7e256feabe234e702c308ff20a9e2d9
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2025-01-13 00:01:35 +0700

    add scan history ui

--- components/HistoryCard.tsx
+++ components/HistoryCard.tsx
@@ -2,14 +2,16 @@ import ScanHistory from "@/app/scanHistory";
 import { ScanHistoryProp } from "@/constants/types";
 import { View, Text, Image } from "react-native";
 
-export default function HistoryCard({ item }: { item: ScanHistoryProp}){
+export default function HistoryCard({ item }: { item: ScanHistoryProp }){
     return (
-        <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
-            <Image source={{ uri: item.imageUrl }} style={{ width: 50, height: 50, borderRadius: 25 }} />
-            <View style={{ marginLeft: 10 }}>
-                <Text style={{ fontWeight: "bold" }}>{item.sumberSampah}</Text>
-                <Text>{item.kategoriSampah}</Text>
-                <Text>{item.createdAt.toLocaleString()}</Text>
+        <View style={{ borderColor: "#E0E0E0", borderWidth: 2, borderRadius: 12, padding: 4, marginHorizontal: 12, marginTop: 6, shadowColor: "#000", shadowOffset: { width: 2, height: 2 }, shadowOpacity: 0.1, shadowRadius: 4 }}>
+            <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
+                <Image source={{ uri: item.imageUrl }} style={{ width: 50, height: 50, borderRadius: 25 }} />
+                <View style={{ marginLeft: 10 }}>
+                    <Text style={{ fontWeight: "bold" }}>{item.sumberSampah}</Text>
+                    <Text>{item.kategoriSampah}</Text>
+                    <Text>{item.createdAt.toLocaleString()}</Text>
+                </View>
             </View>
         </View>
     )
