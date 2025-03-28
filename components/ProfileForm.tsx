77b3959360fc28c3a14117630934e0585fce6055
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2025-03-29 02:47:26 +0700

    nh edit profile page

--- components/ProfileForm.tsx
+++ components/ProfileForm.tsx
@@ -1,10 +1,30 @@
-import { View, TouchableOpacity, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { View, TouchableOpacity, Text, StyleSheet, ActivityIndicator, Modal, FlatList } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import TextInputField from "@/components/TextInputField";
 import useUserData from "@/hooks/useUserData";
+import { useEffect, useState } from "react";
+import { Picker } from "@react-native-picker/picker";
+import { countries } from "@/data/countries";
 
 export default function ProfileForm() {
     const { userData, loading } = useUserData();
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        dob: "",
+        country: "",
+    })
+
+    useEffect(() => {
+        if (userData){
+            setFormData({
+                name: userData.name || "",
+                email: userData.email || "",
+                dob: userData.dob || "",
+                country: userData.country || "",
+            });
+        }
+    }, [userData]);
 
     if (loading) {
         return (
@@ -16,10 +36,10 @@ export default function ProfileForm() {
 
     return (
         <View style={styles.container}>
-            <TextInputField label="Name" value={userData?.name || ""} />
-            <TextInputField label="Email" value={userData?.email || ""} keyboardType="email-address" />
-            <DropdownField label="Date of Birth" value={userData?.dob || ""} />
-            <DropdownField label="Country/Region" value={userData?.country || ""} />
+            <TextInputField label="Name" value={formData.name} onChangeText={(q) => setFormData({...formData, name: q})}/>
+            <DisabledField label="Email" value={formData.email} />
+            <DropdownField label="Date of Birth" value={formData.dob} disabled />
+            <DropdownField label="Country/Region" value={formData.country} onSelect={(q) => setFormData({...formData, country: q})} />
 
             <TouchableOpacity style={styles.button}>
                 <Text style={styles.buttonText}>Save changes</Text>
@@ -28,14 +48,53 @@ export default function ProfileForm() {
     );
 }
 
-function DropdownField({ label, value }: { label: string; value: string }) {
+function DisabledField({ label, value }: { label: string; value: string }) {
     return (
         <View style={styles.inputContainer}>
             <Text style={styles.label}>{label}</Text>
-            <TouchableOpacity style={styles.inputDropdown}>
+            <View style={[styles.inputDropdown, styles.disabledDropdown]}>
                 <Text>{value}</Text>
+            </View>
+        </View>
+    );
+}
+
+function DropdownField({ label, value, onSelect, disabled = false }: { label: string; value: string, onSelect?: (q: string) => void; disabled?: boolean }) {
+    const [modal, setModal] = useState(false);
+    const [dropdown, setDropdown] = useState(false);
+    const [country, setCountry] = useState(value);
+
+    useEffect(() => {
+            setCountry(value);
+    }, [value]);
+
+    return (
+        <View style={styles.inputContainer}>
+            <Text style={styles.label}>{label}</Text>
+            <TouchableOpacity style={[styles.inputDropdown, disabled && styles.disabledDropdown]} onPress={() => setDropdown((prev) => !prev)} disabled={disabled}>
+                <Text>{value || "Selected Date"}</Text>
                 <Ionicons name="chevron-down" size={16} color="black" />
             </TouchableOpacity>
+
+            {dropdown && (
+                <View style={styles.dropdown}>
+                    <FlatList
+                        data={countries}
+                        keyExtractor={(item) => item.id.toString()}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity 
+                                style={styles.dropdownItem} 
+                                onPress={() => {
+                                    onSelect?.(item.name);
+                                    setDropdown(false);
+                                }}
+                            >
+                                <Text>{item.name}</Text>
+                            </TouchableOpacity>
+                        )}
+                    />
+                </View>
+            )}
         </View>
     );
 }
@@ -82,4 +141,25 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
+    disabledDropdown: {
+        backgroundColor: "#cccccc",
+        borderColor: "#ccc",
+    },
+    disabledText: {
+        color: "#888",
+    },
+    dropdown: {
+        borderWidth: 1,
+        borderColor: "#ddd",
+        borderRadius: 8,
+        backgroundColor: "#fff",
+        marginTop: 5,
+        maxHeight: 150, 
+        overflow: "hidden",
+    },
+    dropdownItem: {
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: "#ddd",
+    },  
 });
